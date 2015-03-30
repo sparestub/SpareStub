29cb0f60fb132a3bd74c685fa250e38433e8d123
sparestub <nick@sparestub.com>
2015-03-30 18:18:02 -0400

    test2

--- sparestub/settings/beta.py
+++ sparestub/settings/beta.py
@@ -20,7 +20,7 @@ STATICFILES_DIRS = (
 
 
 ALLOWED_HOSTS = ['beta.sparestub.com', 'sparestub-beta.herokuapp.com']
-abc
+abcdef
 # Used to create links in emails to our site
 DOMAIN = 'https://beta.sparestub.com'
 
